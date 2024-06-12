835835cef71edf6d5e4545ebe93126d21ec918f3
hanyue1014 <wongxuanbin@gmail.com>
2024-06-12 10:40:51 +0800

    Setup project

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+// generated example on how to use pinia
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
