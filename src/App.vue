5317879776367382f5f23439f0efc2745f7530b4
hanyue1014 <wongxuanbin@gmail.com>
2024-06-12 10:41:30 +0800

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterLink to="/">Home</RouterLink>
-  <RouterLink to="/about">About</RouterLink>
-
   <RouterView />
 </template>
 
-<style scoped>
-a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-</style>
+<style scoped></style>
