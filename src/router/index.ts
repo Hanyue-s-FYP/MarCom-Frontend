77b9cb3707910d0ac44061100e3a7444cc5c2bf7
hanyue1014 <wongxuanbin@gmail.com>
2024-06-14 18:38:03 +0800

    Add routes and dummy views

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,22 +23,110 @@ const router = createRouter({
     {
       path: "/products",
       name: "products",
-      component: () => import("@/views/Dashboard/DashboardView.vue"),
+      component: () => import("@/views/Products/ProductsView.vue"),
+      children: [
+        {
+          path: "",
+          name: "product-list",
+          component: {}, // TODO change this
+        },
+        {
+          path: "new-product",
+          name: "new-product",
+          component: {}, // TODO change this
+        },
+        {
+          path: "product-detail",
+          name: "product-detail",
+          component: {}, // TODO change this
+        },
+        {
+          path: "edit-product",
+          name: "edit-product",
+          component: {}, // TODO change this
+        },
+      ],
     },
     {
       path: "/agents",
       name: "agents",
-      component: () => import("@/views/Dashboard/DashboardView.vue"),
+      component: () => import("@/views/Agents/AgentsView.vue"),
+      children: [
+        {
+          path: "",
+          name: "agent-list",
+          component: {}, // TODO change this
+        },
+        {
+          path: "new-agent",
+          name: "new-agent",
+          component: {}, // TODO change this
+        },
+        {
+          path: "agent-detail",
+          name: "agent-detail",
+          component: {}, // TODO change this
+        },
+        {
+          path: "edit-agent",
+          name: "edit-agent",
+          component: {}, // TODO change this
+        },
+      ],
     },
     {
       path: "/environments",
       name: "environments",
-      component: () => import("@/views/Dashboard/DashboardView.vue"),
+      component: () => import("@/views/Environments/EnvironmentsView.vue"),
+      children: [
+        {
+          path: "",
+          name: "environment-list",
+          component: {}, // TODO change this
+        },
+        {
+          path: "new-environment",
+          name: "new-environment",
+          component: {}, // TODO change this
+        },
+        {
+          path: "environment-detail",
+          name: "environment-detail",
+          component: {}, // TODO change this
+        },
+        {
+          path: "edit-environment",
+          name: "edit-environment",
+          component: {}, // TODO change this
+        },
+      ],
     },
     {
       path: "/simulations",
       name: "simulations",
-      component: () => import("@/views/Dashboard/DashboardView.vue"),
+      component: () => import("@/views/Simulations/SimulationsView.vue"),
+      children: [
+        {
+          path: "",
+          name: "simulation-list",
+          component: {}, // TODO change this
+        },
+        {
+          path: "new-simulation",
+          name: "new-simulation",
+          component: {}, // TODO change this
+        },
+        {
+          path: "simulation-detail",
+          name: "simulation-detail",
+          component: {}, // TODO change this
+        },
+        {
+          path: "edit-simulation",
+          name: "edit-simulation",
+          component: {}, // TODO change this
+        },
+      ],
     },
     {
       path: "/login",
