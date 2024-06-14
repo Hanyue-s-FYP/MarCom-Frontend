917c0d9908e106035d6867dc67d946d1633077ef
hanyue1014 <wongxuanbin@gmail.com>
2024-06-14 15:28:17 +0800

    Simply hook login to dashboard

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
+import { RouterLink, useRouter } from "vue-router";
 import InputGeneric from "@/components/InputGeneric.vue";
+
+const router = useRouter();
+
+const login = () => {
+  // TODO make login logic
+  router.push("/");
+};
 </script>
 
 <template>
   <div class="h-full flex items-center justify-center">
     <form
+      @submit.prevent="login()"
       class="shadow-common bg-white py-6 px-8 flex flex-col gap-4 items-center w-[50%] max-w-96 rounded-[15px]"
     >
       <h1 class="text-center font-bold text-2xl w-full">Log In</h1>
