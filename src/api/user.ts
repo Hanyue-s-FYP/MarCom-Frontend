1daf587affdea37b0795e3413fa7bcd333957d74
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-07-25 14:30:15 +0800

    Add initial updare business profile function

--- src/api/user.ts
+++ src/api/user.ts
@@ -44,3 +44,16 @@ export const getMe = async (): Promise<GetUser> => {
     method: "GET",
   });
 };
+
+export type UpdateBusinessData = BusinessPublicProfile & { ID: number; NewCoverImg?: File };
+
+export const updateBusiness = async (data: UpdateBusinessData): Promise<GeneralResponse> => {
+  const formData = new FormData();
+  const { NewCoverImg, ...left } = data; // extract out the new cover img
+  Object.keys(left).forEach((k) => formData.append(k, data[k as keyof typeof left] as string));
+  if (NewCoverImg) formData.append("NewCoverImg", NewCoverImg);
+  return await callApi("/business", {
+    method: "PUT",
+    data: formData,
+  });
+};
