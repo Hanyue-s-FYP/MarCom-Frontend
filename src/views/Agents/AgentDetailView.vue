bd3e4f1ea75f3280460fcb803f87217cf960beaa
hanyue1014 <wongxuanbin@gmail.com>
2024-08-18 09:16:45 +0800

    Add delete to agent detail, remove get report

--- src/views/Agents/AgentDetailView.vue
+++ src/views/Agents/AgentDetailView.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
-import { getAgentWithSimplifiedEnv } from "@/api/agent";
+import { deleteAgent as deleteAgentAPI, getAgentWithSimplifiedEnv } from "@/api/agent";
 import AgentAttributeDisplay from "@/components/agent/AgentAttributeDisplay.vue";
 import EnvironmentCardSimple from "@/components/EnvironmentCardSimple.vue";
 import type { AgentWithSimulation as AgentWithSimplifiedEnvironment } from "@/types/Agents";
 import { Icon } from "@iconify/vue";
 import { type Ref, ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import ConfirmModal from "@/components/ConfirmModal.vue";
+import { useToasts } from "@/composable/toasts";
 
 const route = useRoute();
 const router = useRouter();
 
 const agent: Ref<AgentWithSimplifiedEnvironment | undefined> = ref();
+const { makeToast } = useToasts();
+const confirmModal: Ref<typeof ConfirmModal | null> = ref(null);
+
+const deleteAgentConfirm = () => {
+  confirmModal.value?.showConfirm();
+};
+const deleteAgent = async () => {
+  if (!agent.value) return;
+  const res = await deleteAgentAPI(agent.value.ID);
+  if (res) {
+    makeToast(res.Message);
+    router.push({ name: "agent-list" });
+  }
+};
 
 onMounted(async () => {
   const res = await getAgentWithSimplifiedEnv(parseInt(route.params.id as string));
@@ -34,17 +50,17 @@ onMounted(async () => {
         <Icon icon="mdi:arrow-left" class="text-[2rem]" />
         <span class="text-xl font-medium">{{ agent?.Name ?? "" }} Details</span>
       </div>
-      <div class="grid grid-cols-3 gap-2 items-center">
-        <button class="btn shadow-common bg-neutral-400 text-white rounded-[10px] px-4 py-2">
-          Get Report
-        </button>
+      <div class="grid grid-cols-2 gap-2 items-center">
         <button
           class="btn-primary shadow-common rounded-[10px] px-4 py-2"
           @click="$router.push({ name: 'edit-agent', params: { id: $route.params.id } })"
         >
           Edit
         </button>
-        <button class="btn shadow-common bg-red-500 text-white rounded-[10px] px-4 py-2">
+        <button
+          class="btn shadow-common bg-red-500 text-white rounded-[10px] px-4 py-2"
+          @click="deleteAgentConfirm"
+        >
           Delete
         </button>
       </div>
@@ -63,5 +79,10 @@ onMounted(async () => {
         <EnvironmentCardSimple v-for="env in agent?.InEnvironments" :key="env.Name" v-bind="env" />
       </div>
     </div>
+    <ConfirmModal
+      ref="confirmModal"
+      @confirm="deleteAgent"
+      content="Deleting this agent will delete everything associated to it"
+    />
   </div>
 </template>
